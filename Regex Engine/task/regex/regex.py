e33ddeb9e85ccd48e6b3a1f5a31ae6076e3bb979
parseltongist <>
2022-01-18 18:22:33 +0100

    Stage 5/6 draft

--- Regex Engine/task/regex/regex.py
+++ Regex Engine/task/regex/regex.py
@@ -29,12 +29,8 @@ def char_match(_regex_char, _char) -> bool:
 
 
 def string_match(_template, _string):
-    #print("Checking:", _template, _string)
-
-
     loop_range = min([len(_template), len(_string)])
-
-    #print("Loop range is: ", loop_range)
+    # print(f"checking {_template, _string}")
 
     for i in range(loop_range):
         try:
@@ -44,93 +40,42 @@ def string_match(_template, _string):
                 if "?" in _template:
                     next_step = 1 if _template[i] == "?" else 2
 
-                    # print("the step value is", next_step)
-
-                    #print(f"trying to match {_template[i + next_step]} and {_string[i]}")
+                    # do not make it recursive
                     next_symbol_check = char_match(_template[i + next_step], _string[i])  # skipping '?' and comparing next mandatory sign, test 'ca?t|cat'
-
-                    # print(f"the result of this try is {next_symbol_check:}")
-
                     if not next_symbol_check:  # problem  that this variable doesn't change it's name
                         return False
                     i += 1
                     if i == loop_range:
                         return True
 
-                    # return True # fixing 1 test but breaks 3
+                    # return full_match(_template[i + next_step::], _string[i::])
 
-                """ * funcitons is not yet done, just copied from above '?' fun"""
-
+                # maybe rethink next_symbol check and inplement below as well?
                 if "*" in _template:
-                    #print("line #63 triggered\n")
-                    # re-write the logic, as steps must be different. example be*|beer  be|br
                     next_step = 1 if _template[i] == "*" else 2
-
-                    # print("Step: ", next_step)
-
-                    # need to check both repeat and 0 scenario:
-
+                    """repeative letter case"""
                     if _template[i-1] == _string[i]:  # repeative letter case
-                        #print("line 76")
                         _string = _string[:i] + _string[i::].strip(_template[i-1])  # cut repittive leters in _string
-
-                        loop_range = min([len(_template), len(_string)])
-                        # try call this the same fun here
-
-                        next_symbol_check = char_match(_template[i + next_step], _string[i])
-                        if not next_symbol_check:
-                            return False
-
-                        i += 1
-                        if i == loop_range:
-                            return True
+                        return full_match(_template[i + next_step::], _string[i::])
 
                     elif _template[i-1] == ".":
-                        #print("LINE 86. case with repitive dot!")
-                        #print("ORIGINAL VALUES: ", _template, _string)
-                        #print(_template[i+1], _string[i+1])
-
-                        # removing repittive signs.
+                        # removing repittive chars.
                         _string = _string[:i+1] + _string[i+1::].strip(_string[i])
 
                         _template = _template[i+1::]
                         _string = _string[i+1::]
 
-                        loop_range = min([len(_template), len(_string)])
 
                         return string_match(_template, _string)
 
-
-
-
-
                     elif _template[i+1] == _string[i]:  # abscent letter case
-                        #print("Line # 81", _template[i-1])
-                        # WORKS
+                        return full_match(_template[i+1::], _string[i::])
 
-                        # CHECK BELOW string if - is ok?
-                        next_symbol_check = char_match(_template[i - next_step], _string[i])
-                        if not next_symbol_check:
-                            return False
-
-                        i += 1
-                        if i == loop_range:
-                            return True
 
                     elif _template[i+1] == _string[i]:  # 0 repititions, (letter presented only once here)
-                        #print('line 86')
-                        pass
+                        return full_match(_template[i + next_step::], _string[i::])
 
-
-                        next_symbol_check = char_match(_template[i + next_step], _string[i])
-                        #print(f"the result of this try is {next_symbol_check:}")
-                        if not next_symbol_check:
-                            return False
-
-                        i += 1
-                        if i == loop_range:
-                            return True
-
+                    i += 1
                 else:  # in none of IFs triggered
                     return False
 
@@ -173,22 +118,18 @@ def full_match(_template, _string, i=0):
 
             if string_match(_template, _string[i:]):
                 return True
-            #print(" increasing counter line # 100  +=1 ")
             i += 1
-
-        # ISSUE WITH THIS LOOP.  STRING_MATCH FUNCTION IS OK. I need to terminate the loop once no longer element in the string
         return False
 
 
 def main():
 
     try:
-        template, string = input().split("|")
-        # template, string = "col.*r|colouuuuuuuuuuur".split("|")
+        #template, string = input().split("|")
+        template, string = "colou?r|colouur".split("|")
     except ValueError:
         print("ERROR! Input should be like a|a. Please try again")
         return main()
-    # template, string = metachar_preprocessing("colou*r|colouur")  # for tests
     template, string = metachar_preprocessing(string, template)
     print(full_match(template, string))
 
@@ -208,18 +149,11 @@ def run_tests():
                  'col.*r$|colors': False,
                  }
 
-    failed_tests = []
     for pair in test_pool.keys():
-        # failed tests at this moment:
-        ['colou?r|color', 'colou?r|colour', 'colou*r|colouur', 'col.*r|colour', 'col.*r|collar']
         template, string = pair.split("|")
-
         template, string = metachar_preprocessing(string, template)
-
-        #print(f"\n\nSTART MATCHING {template} and {string}\n\n")
-
-        # print(full_match(template, string))
-        print(f"{pair} {Fore.GREEN}passed" if full_match(template, string) == test_pool[pair] else f"{pair} {Fore.RED} failed")
+        # print(f"templ: {template}, str: {string}, expected result {test_pool[pair]}, actual result: {full_match(template, string)}")
+        print(f"{pair} {Fore.GREEN}passed" if full_match(template, string) == test_pool[pair] else f"{pair} {Fore.RED}failed")
 
 
 if __name__ == '__main__':
