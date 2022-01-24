f902ddbfc9e793c104d8feec4f903df3a53d124e
parseltongist <>
2022-01-24 18:47:10 +0100

    Stage 6/6 final.

--- Regex Engine/task/regex/regex.py
+++ Regex Engine/task/regex/regex.py
@@ -1,60 +1,60 @@
-class RegexEngine:
-    def __init__(self):
-        pass
+def match(_template, _string) -> bool:
+    """ [1:2] is used instead of [1] to avoid exception when getting out of the given boundaries.
+    for example word = 'a', word[1] -> IndexError, word[1:2] -> ' ' """
 
-    @staticmethod
-    def match(_template, _string) -> bool:
-        if not _template:
-            return True
-        if not _string:
-            return _template == "$" or (len(_template) == 2 and _template[1] in "*?")
+    # empty cases check
+    if not _template:
+        return True
+    if not _string:
+        return _template == "$" or (len(_template) == 2 and _template[1] in "*?")
 
-        if _template[0] == "\\":
-            if _template[1:2] != _string[0]:
-                return False
-            return RegexEngine.match(_template[2:], _string[1:])
-
-        if _template[0] != _string[0] and _template[0] != ".":
-            if _template[1:2] in {"?", "*"}:
-                return RegexEngine.match(_template[2:], _string)
+    """ escaping symbol check """
+    if _template[0] == "\\":
+        if _template[1:2] != _string[0]:
             return False
+        return match(_template[2:], _string[1:])
 
-        match _template[1:2]:
-            case "?":
-                return RegexEngine.match(_template[2:], _string[1:])
+    if _template[0] != _string[0] and _template[0] != ".":
+        if _template[1:2] in {"?", "*"}:
+            return match(_template[2:], _string)
+        return False
 
-            case "*":
-                # _template[2:], _string assumes non repitive char or abscent (in case of "*")
-                # _string[1:] keeps template and
-                # moving _string to the right during each iteration to skip repitetive chars.
-                return RegexEngine.match(_template, _string[1:]) or RegexEngine.match(_template[2:], _string)
+    match _template[1:2]:
+        case "?":
+            return match(_template[2:], _string[1:])
 
-            case "+":
-                return RegexEngine.match(_template, _string[1:]) or RegexEngine.match(_template[2:], _string[1:])
+        case "*":
+            """ _template[2:], _string assumes non repitive char or abscent (in case of "*")
+            _string[1:] keeps template and
+            moving _string to the right during each iteration to skip repitetive chars. """
+            return match(_template, _string[1:]) or match(_template[2:], _string)
 
-        # if all none of the conditions triggered:
-        return RegexEngine.match(_template[1:], _string[1:])
+        case "+":
+            return match(_template, _string[1:]) or match(_template[2:], _string[1:])
 
-    @staticmethod
-    def string_scanner(template, string) -> bool:
-        if not template:
-            return True
+    # if all none of the conditions triggered that means that _template[0] == _string[0]
+    return match(_template[1:], _string[1:])
 
-        if template[0] in "*?+":
-            print("REGEX PATERN ERROR. you can't have '*?+' at the beginning of the string\nTRY AGAIN")
-            return main()
-        if template[0] == "^":
-            return RegexEngine.match(template[1:], string)
-        if RegexEngine.match(template, string):
-            return True
-        if not string:
-            return template == "$"
-        return RegexEngine.string_scanner(template, string[1:])
+
+def string_scanner(template, string) -> bool:
+    if not template:
+        return True
+
+    if template[0] in "*?+":
+        print("REGEX PATERN ERROR. you can't have '*?+' at the beginning of the string\nTRY AGAIN")
+        return main()
+    if template[0] == "^":
+        return match(template[1:], string)
+    if match(template, string):
+        return True
+    if not string:
+        return template == "$"
+    return string_scanner(template, string[1:])
 
 
 def main():
     try:
-        print(RegexEngine.string_scanner(*input().split("|")))  # using asterisk for unpacking
+        print(string_scanner(*input().split("|")))  # using asterisk for unpacking
     except ValueError:
         print("ERROR! Input should be like a|a. Please try again")
         return main()
